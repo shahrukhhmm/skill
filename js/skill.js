b1ba8fde3be0a427cf15bf078ec00bf357c3f441
Shahrukh Mehmood <89530184+shahrukhhmm@users.noreply.github.com>
2022-02-28 14:48:53 +0500

    Add files via upload

--- js/skill.js
+++ js/skill.js
@@ -53,3 +53,29 @@ function fixnav(){
 
 // button
 
+// services
+const body1 = document.querySelector('.body1');
+const body2 = document.querySelector('.body2');
+const body3 = document.querySelector('.body3');
+const body4 = document.querySelector('.body4');
+const body5 = document.querySelector('.body5');
+const inner1 = document.querySelector('.inner1');
+const inner2 = document.querySelector('.inner2');
+const inner3 = document.querySelector('.inner3');
+const inner4 = document.querySelector('.inner4');
+const inner5 = document.querySelector('.inner5');
+body1.addEventListener('click',()=>{
+    inner1.classList.toggle('rotate');
+})
+body2.addEventListener('click',()=>{
+    inner2.classList.toggle('rotate');
+})
+body3.addEventListener('click',()=>{
+    inner3.classList.toggle('rotate');
+})
+body4.addEventListener('click',()=>{
+    inner4.classList.toggle('rotate');
+})
+body5.addEventListener('click',()=>{
+    inner5.classList.toggle('rotate');
+})
